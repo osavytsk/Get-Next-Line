d9a8c79706ae3bb7cca9c1d24c979f3bdaf5245a
osavytsk <33601681+osavytsk@users.noreply.github.com>
2017-12-28 23:00:26 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -13,36 +13,52 @@ int     get_next_line(const int fd, char **line)
     size_t			index;
 	static  char	*tmp;
     int     flag;
+    char    *s;
 
-    ft_strclr(*line);
+//    ft_strclr(*line);
     ft_bzero(buf, BUFF_SIZE + 1);
     ret = 1;
     index = 0;
     flag = 0;
+    *line = NULL;
     if (!line || fd == -1 || BUFF_SIZE < 1)
         return (-1);
-    if (tmp != NULL) {
+    if (tmp != NULL)
+    {
         while (tmp[index] != '\n' && tmp[index] != '\0')
             index++;
         if (tmp[index] == '\n')
             flag = 1;
         *line = ft_strsub(tmp, 0, index);
-        tmp = ft_strsub(tmp, (unsigned int) index + 1, (ft_strlen(tmp) - index));
+        s = ft_strsub(tmp, (unsigned int) index + 1, (ft_strlen(tmp) - index));
+        free(tmp);
+        tmp = s;
         if (flag == 1)
-            return 1;
+            return (1);
     }
     index = 0;
     while (ret > 0)
     {
         ret = (int) read(fd, buf, BUFF_SIZE);
-        if (ft_strchr(buf, '\n') == NULL) 
-            *line = ft_strjoin(*line, buf);
+        if (ft_strchr(buf, '\n') == NULL)
+        {
+            s = ft_strjoin(*line ? *line : "" , buf);
+            if (*line)
+                free(*line);
+            *line = s;
+        }
         else 
         {
             while (buf[index] != '\n' && buf[index] != '\0')
                 index++;
-            *line = ft_strjoin(*line, ft_strsub(buf, 0, index));
-            tmp = ft_strsub(buf, (unsigned int) (index + 1), (ft_strlen(buf) - index));
+            s = ft_strjoin(*line ? *line : "", ft_strsub(buf, 0, index));
+            if (*line)
+                free(*line);
+            *line = s;
+            s = ft_strsub(buf, (unsigned int) (index + 1), (ft_strlen(buf) - index));
+            if (tmp)
+                free(tmp);
+            tmp = s;
 //            if (tmp[0] == '\0' && buf[0] == '\0')
 //                return 0;
 //            else
--- main.c
+++ main.c
@@ -12,14 +12,19 @@ int	main(void){
 
     fd1 = open("/Users/osavytsk/gnl/qwe1.txt", O_RDONLY);
 
-    get_next_line(fd1, &line);
-    printf("%d\n", strcmp(line, "aaa"));
-    get_next_line(fd1, &line);
-    printf("%d\n", strcmp(line, "bbb"));
-    get_next_line(fd1, &line);
-    printf("%d\n", strcmp(line, "ccc"));
-    get_next_line(fd1, &line);
-    printf("%d\n", strcmp(line, "ddd"));
+    while (get_next_line(fd1, &line) > 0) {
+        printf("%s ", line);
+        free(line);
+    }
+//    get_next_line(fd1, &line);
+//    printf("%d\n", strcmp(line, "aaa"));
+//    get_next_line(fd1, &line);
+//    printf("%d\n", strcmp(line, "bbb"));
+//    get_next_line(fd1, &line);
+//    printf("%d\n", strcmp(line, "ccc"));
+//    get_next_line(fd1, &line);
+//    printf("%d\n", strcmp(line, "ddd"));
 	close(fd1);
+//    while (1);
 	return 0;
 }
