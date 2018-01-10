033eb17cd9db46780af7454bcbfe9927c47b6053
osavytsk <33601681+osavytsk@users.noreply.github.com>
2018-01-10 21:50:52 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,5 @@
+
+
 #include "get_next_line.h"
 
 t_gnl *     ft_listing(t_gnl **list, int fd)
@@ -19,8 +21,8 @@ t_gnl *     ft_listing(t_gnl **list, int fd)
     *list = beg;
     our = (t_gnl*)malloc(sizeof(t_gnl));
     our->fd = fd;
-    beg = our;
-    *list = our->next;
+    *list = our;
+    our->next = beg;
     return (our);
 }
 
@@ -33,22 +35,23 @@ t_gnl *     ft_listing(t_gnl **list, int fd)
 
 // }
 
-int         stat_manipul(char **tmp, char ***line, char **s1)
+int         stat_manipul(t_gnl **list, char ***line)
 {
     size_t      index;
     int         flag;
+    char        *s1;
 
     index = 0;
     flag = 0;
 
-    while ((*tmp)[index] != '\n' && (*tmp)[index] != '\0')
+    while ((*list)->tmp[index] != '\n' && (*list)->tmp[index] != '\0')
         index++;
-    if ((*tmp)[index] == '\n')
+    if ((*list)->tmp[index] == '\n')
         flag = 1;
-    **line = ft_strsub(*tmp, 0, index);
-    *s1 = ft_strsub(*tmp, (unsigned int) index + 1, (ft_strlen(*tmp) - index));
-    free(*tmp);
-    *tmp = *s1;
+    **line = ft_strsub((*list)->tmp, 0, index);
+    s1 = ft_strsub((*list)->tmp, (unsigned int) index + 1, (ft_strlen((*list)->tmp) - index));
+    free((*list)->tmp);
+    (*list)->tmp = s1;
     if (flag == 1)
         return (1);
     return (0);
@@ -57,59 +60,52 @@ int         stat_manipul(char **tmp, char ***line, char **s1)
 int     get_next_line(const int fd, char **line)
 {
     static  t_gnl *list;
-    // char    buf[BUFF_SIZE + 1];
-    // int     ret;
     size_t          index;
-    // static  char    *tmp;
     char    *s1;
     char    *s2;
 
     if (!list)
         list = (t_gnl*)malloc(sizeof(t_gnl));
-    ft_listing(&list, fd);
-//go next po list and find fd | if list->fd == fd | return teckyshiy list
-//list vernut' v nachalo
-// elsi nety fd create new list with this fd with malloc(in begin of our lists)
-
-    ft_bzero(buf, BUFF_SIZE + 1);
-    ret = 1;
+    list = ft_listing(&list, fd);
+    ft_bzero(list->buf, BUFF_SIZE + 1);
+    list->ret = 1;
     index = 0;
-    if (!line || fd < 0 || BUFF_SIZE < 1 || read(fd, buf, 0) < 0)
+    if (!line || fd < 0 || BUFF_SIZE < 1 || read(fd, list->buf, 0) < 0)
         return (-1);
     *line = NULL;
-    if (tmp != NULL && tmp[0] != '\0')
-        if (stat_manipul(&tmp, &line, &s1) == 1)
+    if (list->tmp != NULL && list->tmp[0] != '\0')
+        if (stat_manipul(&list, &line) == 1)
             return (1);
-    while (ret > 0 || buf[0] != '\0')
+    while (list->ret > 0 || list->buf[0] != '\0')
     {
-        ret = (int) read(fd, buf, BUFF_SIZE);
-        if (ft_strchr(buf, '\n') == NULL)
+        list->ret = (int) read(fd, list->buf, BUFF_SIZE);
+        if (ft_strchr(list->buf, '\n') == NULL)
         {
-            s1 = ft_strjoin(*line ? *line : "" , buf);
+            s1 = ft_strjoin(*line ? *line : "" , list->buf);
             if (*line)
                 free(*line);
             *line = s1;
-            ft_bzero(buf, BUFF_SIZE + 1);
+            ft_bzero(list->buf, BUFF_SIZE + 1);
         }
         else 
         {
-            while (buf[index] != '\n' && buf[index] != '\0')
+            while (list->buf[index] != '\n' && list->buf[index] != '\0')
                 index++;
-            s1 = ft_strjoin(*line ? *line : "", s2 = (ft_strsub(buf, 0, index)));
+            s1 = ft_strjoin(*line ? *line : "", s2 = (ft_strsub(list->buf, 0, index)));
             free(s2);
             if (*line)
                 free(*line);
             *line = s1;
-            s1 = ft_strsub(buf, (unsigned int)(index + 1), (ft_strlen(buf) - index));
-            if (tmp)
-                free(tmp);
-            tmp = s1;
+            s1 = ft_strsub(list->buf, (unsigned int)(index + 1), (ft_strlen(list->buf) - index));
+            if (list->tmp)
+                free(list->tmp);
+            list->tmp = s1;
                 return (1);
         }
     }
-    if (ret == 0 && *line[0] != '\0')
+    if (list->ret == 0 && *line[0] != '\0')
         return (1);
-    if (ret == 0)
+    if (list->ret == 0)
         return (0);
     return (-1);
 }
--- main.c
+++ main.c
@@ -1,36 +1,42 @@
 #include "get_next_line.h"
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+
+
 
 int	main(void){
 
 	char *line;
 
-	int fd1;
+	int fd1, fd2, fd3;
 
     fd1 = open("/Users/osavytsk/gnl/qwe1.txt", O_RDONLY);
+    fd2 = open("/Users/osavytsk/gnl/qwe2.txt", O_RDONLY);
+    fd3 = open("/Users/osavytsk/gnl/qwe3.txt", O_RDONLY);
 
     while (get_next_line(fd1, &line) > 0) {
         printf("%s\n", line);
         free(line);
     }
 
+    while (get_next_line(fd2, &line) > 0) {
+        printf("%s\n", line);
+        free(line);
+    }
+
+    while (get_next_line(fd3, &line) > 0) {
+        printf("%s\n", line);
+        free(line);
+    }
 
 //    get_next_line(fd1, &line);
 //    printf("%d\n", strcmp(line, "aaa"));
-//    free(line);
 //    get_next_line(fd1, &line);
 //    printf("%d\n", strcmp(line, "bbb"));
-//    free(line);
 //    get_next_line(fd1, &line);
 //    printf("%d\n", strcmp(line, "ccc"));
-//    free(line);
 //    get_next_line(fd1, &line);
 //    printf("%d\n", strcmp(line, "ddd"));
-//    free(line);
-//    while (1);
 	close(fd1);
+//    while (1);
 	return 0;
 }
