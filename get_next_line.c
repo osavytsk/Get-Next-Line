7e04e7206eaa0b6c5426a4dbcbcf116efcfa5df1
osavytsk <33601681+osavytsk@users.noreply.github.com>
2018-01-12 05:12:59 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osavytsk <osavytsk@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/12 03:18:00 by osavytsk          #+#    #+#             */
+/*   Updated: 2018/01/12 03:18:00 by osavytsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-t_gnl *     ft_listing(t_gnl **list, int fd)
+static t_gnl       *ft_listing(t_gnl **list, int fd)
 {
     t_gnl   *beg;
     t_gnl   *our;
@@ -23,83 +35,76 @@ t_gnl *     ft_listing(t_gnl **list, int fd)
     return (our);
 }
 
-//int     buf_manipul()
-// {
-//     char    *s2;
-
-
-// }
-
-int         stat_manipul(t_gnl **list, char ***line)
+static int      buf_manipul(t_gnl **tl, char ***line, char **s1, char **s2)
 {
-    size_t      index;
+    int     i;
+
+    i = 0;
+    if (ft_strchr((*tl)->buf, '\n') == NULL)
+    {
+        *s1 = ft_strjoin(**line ? **line : "", (*tl)->buf);
+        **line ? free(**line) : **line;
+        **line = *s1;
+        ft_bzero((*tl)->buf, BUFF_SIZE + 1);
+        return (0);
+    }
+    else
+    {
+        while ((*tl)->buf[i] != '\n' && (*tl)->buf[i] != '\0')
+            i++;
+        *s2 = (ft_strsub((*tl)->buf, 0, i));
+        *s1 = ft_strjoin(**line ? **line : "", *s2);
+        free(*s2);
+        **line ? free(**line) : **line;
+        **line = *s1;
+        *s1 = ft_strsub((*tl)->buf, (i + 1), (ft_strlen((*tl)->buf) - i));
+        (*tl)->tmp ? free((*tl)->tmp) : (*tl)->tmp;
+        (*tl)->tmp = *s1;
+        return (1);
+    }
+}
+
+static int      stat_manipul(t_gnl **list, char ***line)
+{
+    size_t      i;
     int         flag;
     char        *s1;
 
-    index = 0;
+    i = 0;
     flag = 0;
 
-    while ((*list)->tmp[index] != '\n' && (*list)->tmp[index] != '\0')
-        index++;
-    if ((*list)->tmp[index] == '\n')
+    while ((*list)->tmp[i] != '\n' && (*list)->tmp[i] != '\0')
+        i++;
+    if ((*list)->tmp[i] == '\n')
         flag = 1;
-    **line = ft_strsub((*list)->tmp, 0, index);
-    s1 = ft_strsub((*list)->tmp, index + 1, (ft_strlen((*list)->tmp) - index));
+    **line = ft_strsub((*list)->tmp, 0, i);
+    s1 = ft_strsub((*list)->tmp, i + 1, (ft_strlen((*list)->tmp) - i));
     free((*list)->tmp);
     (*list)->tmp = s1;
-    if (flag == 1)
-        return (1);
-    return (0);
+    return (flag == 1 ? 1 : 0);
 }
 
 int     get_next_line(const int fd, char **line)
 {
-    static  t_gnl *list;
-    t_gnl *tmp_list;
-    size_t          index;
-    char    *s1;
-    char    *s2;
+    static t_gnl    *list;
+    t_gnl           *tl;
+    char            *s1;
+    char            *s2;
 
-    tmp_list = ft_listing(&list, fd);
-    ft_bzero(tmp_list->buf, BUFF_SIZE + 1);
-    tmp_list->ret = 1;
-    index = 0;
-    if (!line || fd < 0 || BUFF_SIZE < 1 || read(fd, tmp_list->buf, 0) < 0)
+    tl = ft_listing(&list, fd);
+    ft_bzero(tl->buf, BUFF_SIZE + 1);
+    tl->ret = 1;
+    if (!line || fd < 0 || BUFF_SIZE < 1 || read(fd, tl->buf, 0) < 0)
         return (-1);
     *line = NULL;
-    if (tmp_list->tmp != NULL && tmp_list->tmp[0] != '\0')
-        if (stat_manipul(&tmp_list, &line) == 1)
+    if (tl->tmp != NULL && tl->tmp[0] != '\0')
+        if (stat_manipul(&tl, &line) == 1)
             return (1);
-    while (tmp_list->ret > 0 || tmp_list->buf[0] != '\0')
+    while (tl->ret > 0 || tl->buf[0] != '\0')
     {
-        tmp_list->ret = (int) read(fd, tmp_list->buf, BUFF_SIZE);
-        if (ft_strchr(tmp_list->buf, '\n') == NULL)
-        {
-            s1 = ft_strjoin(*line ? *line : "" , tmp_list->buf);
-            if (*line)
-                free(*line);
-            *line = s1;
-            ft_bzero(tmp_list->buf, BUFF_SIZE + 1);
-        }
-        else 
-        {
-            while (tmp_list->buf[index] != '\n' && tmp_list->buf[index] != '\0')
-                index++;
-            s1 = ft_strjoin(*line ? *line : "", s2 = (ft_strsub(tmp_list->buf, 0, index)));
-            free(s2);
-            if (*line)
-                free(*line);
-            *line = s1;
-            s1 = ft_strsub(tmp_list->buf, (index + 1), (ft_strlen(tmp_list->buf) - index));
-            if (tmp_list->tmp)
-                free(tmp_list->tmp);
-            tmp_list->tmp = s1;
-                return (1);
-        }
+        tl->ret = (int) read(fd, tl->buf, BUFF_SIZE);
+        if (buf_manipul(&tl, &line, &s1, &s2) == 1)
+            return (1);
     }
-    if (tmp_list->ret == 0 && (*line)[0] != '\0')
-        return (1);
-    if (tmp_list->ret == 0)
-        return (0);
-    return (-1);
+    return tl->ret == 0 && (*line)[0] != '\0' ? 1 : tl->ret == 0 ? 0 : -1;
 }
